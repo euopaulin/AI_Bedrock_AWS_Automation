242baa27b9eeb3d44530548a3cd5bfe0b9f56bea
Paulo Henrique <paulohspfc209@gmail.com>
2025-09-24 10:16:17 -0300

    Atualização do código

--- src/script_AI.py
+++ src/script_AI.py
@@ -1,15 +1,19 @@
 import boto3
 import json
 
-def automa_ia():
+def automa_ia(historico_conversa):
     client = boto3.client('bedrock-runtime', region_name='us-east-1')
     model_id = 'amazon.titan-text-express-v1'
 
     body = {
-        "inputText": "Olá, meu nome é Paulo. Como você está?",
+        #Antes estava sem "Human e Assistant", o que fazia o modelo responder de forma errada.
+        #Com human e assistant, o modelo entendeu que se trata de uma conversa entre humano 
+        #e IA e não precisa repetir o output a minha frase, somente a frase dele.
+        "inputText": historico_conversa,
         "textGenerationConfig": {
+            #Após reduzir os tokens, o modelo respondeu corretamente.
             "maxTokenCount": 200,
-            "temperature": 0.5,
+            "temperature": 0.2,
         }
     }
 
@@ -30,4 +34,22 @@ def automa_ia():
         print(f"Ocorreu um erro: {e}")
 
 if __name__ == "__main__":
-    automa_ia()
+    historico = "Human: Olá, IA!"
+    print(f"Inicio do chat. Por favor, digite 'sair' para encerrar a conversa.")
+
+    while True:
+        entrada = input("Você: ")
+        if entrada.lower() == "sair":
+            print("Encerrando o chat.")
+            break
+        
+        novo_prompt = historico + f" Human: {entrada} Assistant:"
+        resposta_ia = automa_ia(novo_prompt)
+        
+        resposta_ia = automa_ia(historico)
+        if resposta_ia:
+            historico += f" {resposta_ia}"
+            print(f"IA: {resposta_ia}")
+        else:
+            print(f"Não foi possível obter uma resposta da IA.")
+            break
